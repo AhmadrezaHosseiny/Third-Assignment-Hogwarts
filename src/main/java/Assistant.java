13e0ef06ece2cade778890d2b5ec8dadd45fd4b1
Ahmad reza Hosseiny <a.r.hosseiny1384@gmail.com>
2024-03-24 15:06:11 +0330

    fixed

--- src/main/java/Assistant.java
+++ src/main/java/Assistant.java
@@ -2,7 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/* The assistant serves as an administrative tool, allowing administrators to create accounts, manage users,
+ courses, and perform various administrative tasks to ensure smooth operation of the platform.
+ */
 public class Assistant {
+
+
+
     // Create a Course: Follow steps to create a new course on the platform:
     //                  Navigate to the course creation section in the dashboard.
     //                  Provide necessary details for the new course.
@@ -13,8 +19,7 @@ public class Assistant {
         String title = reader.readLine();
         System.out.println("Save? [Y/N]");
         String response = reader.readLine();
-        if (!response.equals("n")) {
-
+        if (!response.equals("n") && !response.equals("N")) {
             Course c = new Course();
             c.set_title(title);
             Hogwarts.courses.add(c);
